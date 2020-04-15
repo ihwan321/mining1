693ddae3f7682a3411473132c7afc3f33ba31e7c
ihwan321 <63368199+ihwan321@users.noreply.github.com>
2020-04-15 09:12:23 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*Optional*/
+/*Boleh Diganti, Boleh Tidak*/
+
+$boost_url = "av=1.0.0.98&v=4.0.11&bid=981dcc5d-266b-4cee-a567-6bd29fa6dc8e&app_id=pro.cryptotab.android";
+$start = "bid=981dcc5d-266b-4cee-a567-6bd29fa6dc8e&app_id";
+$versi = "av=1.0.0.98&v=4.0.11&bid=981dcc5d-266b-4cee-a567-6bd29fa6dc8e";
+$data_boost = '{"boost_ratio":2}';
+
+/*Change According to Your Auth Data*/
+/*Ubah Sesuai Data Auth Kalian*/
+
+$auth[] = "gAAAAABelhzL1-pY3ptn8Sn74LbJeFaHmwuwpqwctvzKn_EDQvNnCr6NeMyE3NNbPI7__WCqLCpzfqeVzEb8RYs4xTTupP0sDYayUH6ou6HjhRtDEamosSQ=";
+$auth[] = "gAAAAABelhRC0Q8G2jT32PCmA2AbIc0L_BG8SRkpfnRKzxOZWBIDb6ZoJjKw0hKGsKea5kGfFek8Jwp8X_Wk82kboMWv-hq7IEVN8Tpm1QIxGDZ8Gq8ZZy8=";
